ef4f42eaca20e5fbcbd94817b84608cd11e3c2fc
Gregoire Pelegrin <peleggreg@gmail.com>
2020-07-13 19:01:03 +0200

    Adding the credentials, next step is to dl the library

---
+++ APITests.py
@@ -0,0 +1,12 @@
+API_key = "5h3nAtnW3Ko11PoS4c33QdZIc"
+API_secret_key = "cx43frNHofxE9fKiz6E9JWltb9qXujvKDiaPgPKs5Cop22prBo"
+Bearer_token = "AAAAAAAAAAAAAAAAAAAAAEzsFwEAAAAAYqTAZ1IVH0JrFFlQK0CZureL6dM%3D9b1aSqu3N4g4UcYUFaeqWBTArxLZI4QVn4ZrqT9GkRH1zGcwNW"
+Access_token = "2166565049-uPnSHapwSZv86lwSK5VS4bUfFeXfrRNoknURGOU"
+Access_secret_token = "NqEVHIgdUV5fYHGfj56vAiq7GRaOCNCdGTRWENIpKKiQ4"
+
+credentials = {}
+credentials['API_KEY'] = API_key
+credentials['API_SECRET'] = API_secret_key
+credentials['BEARER_TOKEN'] = Bearer_token
+credentials['ACCESS_TOKEN'] = Access_token
+credentials['ACCESS_SECRET'] = Access_secret_token
